75e034e3009fd775f2f0e2dc56bf9ba96d4b11b3
bohaesoju <bohaesoju@gmail.com>
2019-11-09 19:22:15 +0900

    setting local storage

--- src/components/ChattingRoom.jsx
+++ src/components/ChattingRoom.jsx
@@ -31,8 +31,20 @@ export const ChattingRoom = () => {
 
             chatHistory.push(message);
             setChattingMessage('');
+            localStorage.setItem("chat", JSON.stringify(chatHistory));
 
-            console.log(chatHistory, message)
+            let localData = localStorage.getItem("chat");
+            let localDataJson = JSON.parse(localData);
+
+            let i = 0;
+            let templateDiv =
+                "<li class='leftSide talkMessage'>" +
+                "<p className='about'>" + localDataJson[i].text +
+                "</p>" +
+                "</li>";
+            i++;
+
+            console.log(templateDiv, message.text)
         }
 
         // setChattingMessage('');
@@ -46,39 +58,9 @@ export const ChattingRoom = () => {
         <div className="ChattingRoom">
             <ul className="chattingMessageWrap">
                 <li className="leftSide talkMessage">
-                    <div className="about">
-                        유이야 안녕? 어떻게 지내?
-                    </div>
-                </li>
-                <li className="leftSide talkMessage">
-                    <div className="about">
-                        잘지내고 있는거지??
-                    </div>
-                </li>
-                <li className="rightSide talkMessage">
-                    <div className="about">
-                        응응. 나는 잘 지내고 있지 오랜만이야
-                    </div>
-                </li>
-                <li className="leftSide talkMessage">
-                    <div className="about">
-                        우리 얼굴 본지도 오래된것 같다.
-                    </div>
-                </li>
-                <li className="leftSide talkMessage">
-                    <div className="about">
-                        한번 봐야 하는데ㅎㅎ
-                    </div>
-                </li>
-                <li className="rightSide talkMessage">
-                    <div className="about">
-                        응 맞아 언제 일마치고 한번 보자 !
-                    </div>
-                </li>
-                <li className="leftSide talkMessage">
-                    <div className="about">
-                        그래 보고싶다
-                    </div>
+                    <p className='about'>
+                        { chattingMessage }
+                    </p>
                 </li>
             </ul>
             <div className="writeMessageArea">
